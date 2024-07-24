36ef89faea97b489231da94dfc9cef594ca30ee8
Manasse Nkundimana <musoniroger5@gmail.com>
2024-07-24 15:24:29 -0400

    IoC and Context

--- src/main/java/emp/Employee.java
+++ src/main/java/emp/Employee.java
@@ -1,58 +1,38 @@
 package emp;
 
+import java.util.List;
+import java.util.Map;
+
 public class Employee {
 	private int empId;
 	private String name;
 	private int age;
 	private Address address;
+	private List<Task> tasks;
+	private Map<Integer, String> manager;
 	
 	public Employee() {
 	}
 	
-	public Employee(int empId, String name, int age, Address address) {
+	public Employee(int empId, String name, int age, Address address, List<Task> tasks, Map<Integer, String> manager) {
 		super();
 		this.empId = empId;
 		this.name = name;
 		this.age = age;
 		this.address = address;
-	}
-	
-	public int getEmpId() {
-		return empId;
-	}
-	
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public Address getAddress() {
-		return address;
+		this.tasks = tasks;
+		this.manager = manager;
 	}
 
-	public void setAddress(Address address) {
-		this.address = address;
-	}
-
-	
 	public void printValues() {
-		System.out.println("empId=" + empId + ", name=" + name + ", age=" + age+", address="+address.getStreet()+" "+address.getCity());
+		System.out.println("id: "+empId);
+		System.out.println("name: "+name);
+		System.out.println("address: ["+address.getStreet()+" "+address.getCity()+" "+address.getState()+"]");
+		System.out.println("tasks:");
+		tasks.stream().forEach(System.out::println);
+		System.out.println("manager: ");
+		for (Map.Entry<Integer,String> entry : manager.entrySet())  
+            System.out.println("Id = " + entry.getKey()+ ", Name = " + entry.getValue()); 
 	}
-	
-	
 	
 }
--- src/main/java/emp/Main.java
+++ src/main/java/emp/Main.java
@@ -7,10 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
 		Employee emp1=(Employee) context.getBean("empbean1");
-		Employee emp2=(Employee) context.getBean("empbean2");
-		Employee emp3=(Employee) context.getBean("empbean3");
+//		Employee emp2=(Employee) context.getBean("empbean2");
+//		Employee emp3=(Employee) context.getBean("empbean3");
 		emp1.printValues();
-		emp2.printValues();
-		emp3.printValues();
+//		emp2.printValues();
+//		emp3.printValues();
 	}
 }
