f9e55ac028b0f3b3154c5c23b6d4325f4a6a3b0c
Manasse Nkundimana <musoniroger5@gmail.com>
2024-07-25 14:48:01 -0400

    inheritance beans

--- src/main/java/emp/Employee.java
+++ src/main/java/emp/Employee.java
@@ -10,10 +10,10 @@ public class Employee {
 	private Address address;
 	private List<Task> tasks;
 	private Map<Integer, String> manager;
-	
+
 	public Employee() {
 	}
-	
+
 	public Employee(int empId, String name, int age, Address address, List<Task> tasks, Map<Integer, String> manager) {
 		super();
 		this.empId = empId;
@@ -25,14 +25,14 @@ public class Employee {
 	}
 
 	public void printValues() {
-		System.out.println("id: "+empId);
-		System.out.println("name: "+name);
-		System.out.println("address: ["+address.getStreet()+" "+address.getCity()+" "+address.getState()+"]");
+		System.out.println("id: " + empId);
+		System.out.println("name: " + name);
+		System.out.println("address: [" + address.getStreet() + " " + address.getCity() + " " + address.getState() + "]");
 		System.out.println("tasks:");
 		tasks.stream().forEach(System.out::println);
 		System.out.println("manager: ");
-		for (Map.Entry<Integer,String> entry : manager.entrySet())  
-            System.out.println("Id = " + entry.getKey()+ ", Name = " + entry.getValue()); 
+		for (Map.Entry<Integer, String> entry : manager.entrySet())
+			System.out.println("Id = " + entry.getKey() + ", Name = " + entry.getValue());
 	}
-	
+
 }
